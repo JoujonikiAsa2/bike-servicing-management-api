9c443654582157fda87e738ecb4d1a0736c705f2
JoujonikiAsa2 <nibeditaroy582@gmail.com>
2025-04-16 23:27:50 +0600

    feat: update create api response status

--- src/app/modules/customer/customer.controller.ts
+++ src/app/modules/customer/customer.controller.ts
@@ -2,11 +2,12 @@ import { Request, Response } from "express";
 import { asyncWrapper } from "../../../shared/asyncWrapper";
 import { sendResponse } from "../../../shared/sendResponse";
 import { CustomerService } from "./customer.service";
+import httpStatus from "http-status";
 
 const createCustomer = asyncWrapper(async (req, res) => {
     const result = await CustomerService.createCustomer(req.body);
     sendResponse(res, {
-      statusCode: 200,
+      statusCode: httpStatus.CREATED,
       success: true,
       message: "Customer created successfully",
       data: result,
